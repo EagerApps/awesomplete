15a5caa8d1cf949a92f141a429c85dd15eed4b17
Zack Bloom <zackbloom@gmail.com>
2015-02-21 01:32:40 -0500

    Make installable

---
+++ install/helper.js
@@ -0,0 +1,22 @@
+(function(){
+
+  var elements = INSTALL_OPTIONS.elements;
+  for (var i=0; i < elements.length; i++){
+    var origSugg = elements[i].suggestions.split(',');
+    var suggestions = [];
+    for (var j=0; j < origSugg.length; j++){
+      suggestions.push(origSugg[j].trim());
+    }
+
+    var input = document.querySelector(elements[i].input);
+
+    if (input && suggestions.length) {
+      new Awesomplete(input, {
+        list: suggestions,
+        minChars: 1,
+        autoFirst: elements[i].auto
+      });
+    }
+  }
+
+})()
